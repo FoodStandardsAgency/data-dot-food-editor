8abf1cb225e7cfa2bb51e5d91bfc14f67c70bca6
Michael Waterworth <mike@3equals.co.uk>
2016-12-12 15:34:39 +0000

    Remove JSX

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -70,16 +70,6 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.jsx$/,
-        loader: 'babel',
-        // include: projectRoot,
-        exclude: /node_modules(?!\/(vue-tables-2|vue-pagination-2))/
-        // query: {
-        //   plugins: ["transform-vue-jsx"],
-        //   presets: ["es2015"]
-        // }
-      },
-      {
         test: /\.json$/,
         loader: 'json'
       },
