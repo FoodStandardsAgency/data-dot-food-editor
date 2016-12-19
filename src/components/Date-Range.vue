8d94160130efbb68830199bc5abcd6c99e9c29a6
Mike Waterworth <mike.waterworth@epimorphics.com>
2016-12-19 15:24:40 +0000

    Return infinity if underfined start of end date vals

--- src/components/Date-Range.vue
+++ src/components/Date-Range.vue
@@ -24,26 +24,30 @@ Also supports an empty text value
           return '∞'
         }
         let refDate = new Date()
+        let changed = false
         this.arr.forEach((item) => {
           let itemDate = moment(item[this.startProp], dateFormat)
           if (itemDate < refDate) {
             refDate = itemDate
+            changed = true
           }
         })
-        return refDate
+        return changed ? refDate : '∞'
       },
       lastDate () {
         if (!this.arr || !this.arr.length) {
           return '∞'
         }
         let refDate = new Date('1970-1-1')
+        let changed = false
         this.arr.forEach((item) => { // Loop through to dates
           let itemDate = moment(item[this.endProp], dateFormat)
           if (itemDate > refDate) {
             refDate = itemDate // Set as current largest
+            changed = true
           }
         })
-        return refDate // Return current largest
+        return changed ? refDate : '∞'
       }
     }
   }
