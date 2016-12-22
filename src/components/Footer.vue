8ddb94d67f58f1274ae5d3dd6d9cf25aa58b25be
Mike Waterworth <mike.waterworth@epimorphics.com>
2016-12-22 11:15:50 +0000

    Increase the size of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,11 +4,17 @@ Template footer
 <template>
   <footer>
     <div class="container">
-      For help and support, please contact <a href="mailto:support@epimorphics.com">Epimorphics Ltd</a>
+      Developed by Epimorphics Ltd <br/>
+      For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a>
     </div>
   </footer>
 </template>
 
 <script></script>
 
-<style></style>
+<style scoped>
+  footer {
+    padding: 10px 0;
+    text-align: center;
+  }
+</style>
