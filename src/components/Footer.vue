13616c6c9acb43848ee446df119b792a13eff939
Mike Waterworth <mike.waterworth@epimorphics.com>
2016-12-22 13:38:36 +0000

    Add epimorphics logo to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,11 @@ Template footer
 <template>
   <footer>
     <div class="container">
-      Developed by Epimorphics Ltd <br/>
-      For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a>
+      <img src="../assets/epimorphics.png" class="epimorphics-logo" alt="Epimorphics">
+      <div>
+        Linked-data catalogue editor by Epimorphics Ltd &copy; 2016 <br/>
+        For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a>
+      </div>
     </div>
   </footer>
 </template>
@@ -16,5 +19,9 @@ Template footer
   footer {
     padding: 10px 0;
     text-align: center;
+  }
+  .epimorphics-logo {
+    width: 50px;
+    margin: 0 10px;
   }
 </style>
