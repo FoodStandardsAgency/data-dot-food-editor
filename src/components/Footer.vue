a7ccb366153a58c2aeaf9d3726c4beab47ae7535
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-01-26 14:50:15 +0000

    Display latest year for copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@ Template footer
     <div class="container">
       <img src="../assets/epimorphics.png" class="epimorphics-logo" alt="Epimorphics">
       <div>
-        Linked-data catalogue editor by Epimorphics Ltd &copy; 2016 <br/>
+        Linked-data catalogue editor by Epimorphics Ltd &copy; {{ new Date().getFullYear() }} <br/>
         For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a>
       </div>
     </div>
