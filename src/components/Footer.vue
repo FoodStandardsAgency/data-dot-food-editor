4163bbfba5e053c74ec7353b5b3fbdb88e2ad7ba
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-01-31 11:36:20 +0000

    Update footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,9 @@ Template footer
     <div class="container">
       <img src="../assets/epimorphics.png" class="epimorphics-logo" alt="Epimorphics">
       <div>
-        Linked-data catalogue editor by Epimorphics Ltd &copy; {{ new Date().getFullYear() }} <br/>
-        For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a>
+        Data catalog editor by Epimorphics Ltd, linked-data technology <br/>
+        For help and support, please contact <a href="mailto:support@epimorphics.com">Support</a> <br/>
+        &copy; {{ new Date().getFullYear() }}
       </div>
     </div>
   </footer>
