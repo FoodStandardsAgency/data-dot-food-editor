25d4ae783c85bd573689713d352f002fe79a9ba4
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-02-16 15:51:07 +0000

    Message event binding

--- src/Element-Details.vue
+++ src/Element-Details.vue
@@ -20,10 +20,11 @@ Displayed as a modal
             <a v-if="$route.params.eid !== 'undefined'" class="btn btn-danger" @click="remove">Delete</a>
             <router-link :to="{ name: 'elements', params: { id: $route.params.id, eid: 'new' }}" class="btn btn-default">Cancel</router-link>
             <button @click="save" type="button" class="btn btn-success">Save</button> <!-- :disabled="!unsavedChanges ? true : false" -->
+            <button @click="createMessage" type="button" class="btn btn-success">New message</button> <!-- :disabled="!unsavedChanges ? true : false" -->
           </div>
           <h4 class="modal-title" id="myModalLabel">Element editor</h4>
 
-          <messages :success="successMsg" :warn="warnMsg"></messages>
+          <messages v-on:message="incrementTotal" :success="successMsg" :warn="warnMsg"></messages>
 
           <div class="form-group" style="clear:left">
             <label for="Eltitle">Title</label>
@@ -126,6 +127,9 @@ Displayed as a modal
       })
     },
     methods: {
+      createMessage () {
+        this.$emit('message', 'message content')
+      },
       fetchData () {
         if (this.$route.query.saved) {
           this.successMsg = 'Added Successfully'
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -4,19 +4,60 @@ Show messages from the page in a standard format
 -->
 <template>
   <div class="messages">
-    <div class="message successMsg bg-success" v-if="success">{{success}}</div>
-    <div class="message warnMsg bg-danger" v-if="warn">{{warn}}</div>
+    <transition name="fade">
+      <div class="message successMsg bg-success" v-if="showSuccess">{{success}}</div>
+    </transition>
+    <transition name="fade">
+      <div class="message warnMsg bg-danger" v-if="showWarn">{{warn}}</div>
+    </transition>
   </div>
 </template>
 
 <script>
+  let successTimeout
+  // let warnTimeout
   export default {
+    created () {
+      this.$on('message', function (msg) {
+        console.log('New Message', msg)
+      })
+    },
     props: {
       success: String,
       warn: String
+    },
+    data: function () {
+      return {
+        showSuccess: false,
+        showWarn: false
+      }
+    },
+    watch: {
+      success (oldVal) {
+        console.log('show success')
+
+        this.showSuccess = true
+        if (successTimeout) {
+          clearTimeout(successTimeout)
+        }
+        successTimeout = setTimeout(() => {
+          console.log('hide success')
+          this.showSuccess = false
+          this.success = ''
+        }, 1000)
+      },
+      warn (oldVal) {
+        // warnTimeout = new Date().getTime()
+      }
     }
   }
 </script>
 
 <style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+    opacity: 0
+  }
 </style>
