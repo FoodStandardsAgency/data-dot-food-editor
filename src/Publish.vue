05a616bd0d948632231db8167b71746030804fe7
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-02-20 14:40:51 +0000

    Update published text

--- src/Publish.vue
+++ src/Publish.vue
@@ -8,9 +8,12 @@ Call endpoint to start push to public servers
       <div class="pull-right" style="margin-top:15px;">
       </div>
       <h1>Publish</h1>
+      <button class="btn btn-lg btn-success pull-right" @click="publish">Publish</button>
+
+      <h3>Copy changes from editor to public browser</h3>
       <messages/>
-      <p>Nullam ut tincidunt nunc. [Pellentesque][1] metus lacus, commodo eget justo ut, rutrum varius nunc. Sed non rhoncus risus. Morbi sodales gravida pulvinar. Duis malesuada, odio volutpat elementum vulputate, massa magna scelerisque ante, et accumsan tellus nunc in sem. Donec mattis arcu et velit aliquet, non sagittis justo vestibulum. Suspendisse volutpat felis lectus, nec consequat ipsum mattis id. Donec dapibus vehicula facilisis. In tincidunt mi nisi, nec faucibus tortor euismod nec. Suspendisse ante ligula, aliquet vitae libero eu, vulputate dapibus libero. Sed bibendum, sapien at posuere interdum, libero est sollicitudin magna, ac gravida tellus purus eu ipsum. Proin ut quam arcu.</p>
-      <button @click="publish">Publish</button>
+      <p>Click publish to start the process of publishing the current editor datasets to the public browse site. </p>
+      <p>This process can take up to 10 minutes, please be patient. </p>
     </div>
   </main>
 </template>
