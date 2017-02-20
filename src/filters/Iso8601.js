1cdfd7b1a8937b793cdb2158b5b4a55a8e4e6a44
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-02-20 14:52:20 +0000

    Re-add renamed files

---
+++ src/filters/Iso8601.js
@@ -0,0 +1,7 @@
+import iso8601 from '../iso8601'
+export default function (d) {
+  if (d) {
+    let parsed = iso8601.Period.parseToString('P' + d)
+    return 'Typically published every ' + (parsed || '...')
+  }
+}
