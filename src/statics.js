e26020dcf24e3882939053ee15275ef43e7233f3
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-03-06 16:08:03 +0000

    Externalise keyword URI incase it needs to change

--- src/statics.js
+++ src/statics.js
@@ -1,3 +1,4 @@
 export default {
-  distributionUri: 'http://data.food.gov.uk/catalog/distribution/'
+  distributionUri: 'http://data.food.gov.uk/catalog/distribution/',
+  keywordUri: 'http://data.food.gov.uk/catalog/data/keyword/'
 }
