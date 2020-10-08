6d629e54b3d2267f2199ba0ee90e3b3066fe9447
bogdan <marc.bogdan96@yahoo.com>
2020-10-08 11:37:26 +0100

    reconfigured proxy to fix some 404 errors

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/catalog/': {
+      '/catalog/editor/': { // baseEndpoint
+        target: 'https://fsa-dev-catalog-editor.epimorphics.net'
+      },
+      '/catalog/system/security/': { // securityEndpoint
+        target: 'https://fsa-dev-catalog-editor.epimorphics.net'
+      },
+      '/catalog-editor/static/api/Datatypes.json': { // datatypesEndpoint
         target: 'https://fsa-dev-catalog-editor.epimorphics.net'
       }
     }
