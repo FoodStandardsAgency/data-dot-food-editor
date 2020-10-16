0918a6601defbaf1d00060ab1f1db824fd147412
bogdan <marc.bogdan96@yahoo.com>
2020-10-16 17:13:38 +0100

    added the help view
    https://github.com/FoodStandardsAgency/data-dot-food/issues/16
    
    This adds the help page view; a route and a link to it are needed in future commits to make it accessible

---
+++ src/Help.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>Help</h1>
+      <h3>1. About this page</h3>
+      <p>This page is currently a draft and contains temporary content.</p>
+      <p>In the future this will contain information about the website, likely a FAQ section, and a table of contents area.</p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
